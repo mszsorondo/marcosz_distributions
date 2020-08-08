beaeeab2b429dbbe64fdfb0702abaec5ca656bd4
msorondo <msorondo@live.com.ar>
2020-08-08 19:04:19 -0300

    asd

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='marcosz_distributions',
-      version='0.1',
-      description='Gaussian distributions',
-      packages=['marcosz_distributions'],
-      zip_safe=False)
